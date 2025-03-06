26a2a0025b0a9ae4753e70f65511b75742dfeac1
Malhar <mcb110104@gmail.com>
2025-03-06 21:26:55 +0530

    Update Register for Mentor

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -254,7 +254,7 @@ const Register = () => {
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
                                         </svg>
                                     </div>
-                                    <h3 className="text-2xl font-bold text-green-700">Teacher</h3>
+                                    <h3 className="text-2xl font-bold text-green-700">Mentor</h3>
                                     <p className="text-green-600 mt-2">Share your knowledge</p>
                                 </motion.div>
                             </div>
